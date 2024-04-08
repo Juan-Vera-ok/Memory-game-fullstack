bee1b6825169f6fe0b984751a398fa049ad6b621
Nicolas Oga <2112.oga@gmail.com>
2024-04-08 00:13:04 -0300

    chchch changes to the backend, indent, JWT_SECRET

--- Back/src/routes/user.routes.ts
+++ Back/src/routes/user.routes.ts
@@ -1,20 +1,12 @@
-import {Router} from 'express'
-import * as userController from './users.controllers';
-import checkAuth from '../middlewares/isAuth';
-
+import {RequestHandler, Router} from 'express'
 const router = Router();
-router.use(checkAuth)
-router.post('/user',userController.createUser);
 
-router.get('/users',userController.getUsers);
+import * as userController from './users.controllers';
 
-router.post('/userAuth',userController.auth)
-
-router.delete('/user/:id',userController.deleteUser);
-
-router.put('/user/:id',userController.updateUser);
-
-
-
+router.post('/user', userController.createUser);
+router.get('/users', userController.getUsers);
+router.post('/userAuth', userController.auth);
+router.delete('/user/:id', userController.deleteUser);
+router.put('/user/:id', userController.updateUser);
 
 export default router
--- Back/src/routes/users.controllers.ts
+++ Back/src/routes/users.controllers.ts
@@ -4,73 +4,73 @@ import User from "./User";
 import bcryptNow from "../helpers/handleBcrypt";
 import { compare } from "bcryptjs";
 import { token } from "morgan";
-import{ tokenSign} from "../helpers/generateToken";
+import { tokenSign } from "../helpers/generateToken";
 
-export const createUser: RequestHandler = async (req,res) =>{
-    
+export const createUser: RequestHandler = async (req, res) => {
+
     try {
-        const userFound = await User.findOne({user: req.body.user})
-    if(userFound){
-        return res.status(301).json({message:'El usuario ya existe'})
-    }
-    const {email,password,user} = req.body;
-    const passwordHashed =  bcryptNow.encrypt(password);
-    const newUser = {email,passwordHashed,user}
-    const registerUser = new User(newUser)
-    const savedUser = await registerUser.save()
-    res.json(savedUser)
-        
-        
-    
-    
+        const userFound = await User.findOne({ user: req.body.user })
+        if (userFound) {
+            return res.status(301).json({ message: 'El usuario ya existe' })
+        }
+        const { email, password, user } = req.body;
+        const passwordHashed = bcryptNow.encrypt(password);
+        const newUser = { email, passwordHashed, user }
+        const registerUser = new User(newUser)
+        const savedUser = await registerUser.save()
+        res.json(savedUser)
+
+
+
+
     } catch (error) {
         console.log(error)
     }
 }
 
-export const getUsers: RequestHandler = async (req,res) =>{
-    try{
+export const getUsers: RequestHandler = async (req, res) => {
+    try {
         const users = await User.find();
-    res.json(users)
-    }catch(error){
+        res.json(users)
+    } catch (error) {
         res.json(error)
     }
 }
-export const auth: RequestHandler = async (req,res) =>{
+export const auth: RequestHandler = async (req, res) => {
     try {
-        
+
         console.log(req.body.data)
-        const userFound= await User.findOne({user: req.body.data.username})
-        console.log("XDDDD"+userFound);
-        if(userFound)
-        {const correctPassword = await bcryptNow.compare(req.body.data.password,userFound?.passwordHashed)
-        
-            if(correctPassword){
+        const userFound = await User.findOne({ user: req.body.data.username })
+        console.log("XDDDD" + userFound);
+        if (userFound) {
+            const correctPassword = await bcryptNow.compare(req.body.data.password, userFound?.passwordHashed)
+
+            if (correctPassword) {
                 const tokenSession = await tokenSign(userFound)
-                res.cookie("token",tokenSession)
-                
+                res.cookie("token", tokenSession)
+
                 res.json(200)
             }
-                
+
         }
-            if(!userFound){
-                console.log("SSSSSSSSSSSSSSSSSSS")
-                res.json(301)
-            }
-        
-        
+        if (!userFound) {
+            console.log("SSSSSSSSSSSSSSSSSSS")
+            res.json(301)
+        }
+
+
     } catch (error) {
         console.log(error)
     }
 }
 
-export const deleteUser: RequestHandler = async (req,res) =>{
+export const deleteUser: RequestHandler = async (req, res) => {
     const userFound = await User.findByIdAndDelete(req.params.id);
-    if(!userFound) { return res.status(204).json();}
+    if (!userFound) { return res.status(204).json(); }
     return res.json(userFound)
 }
 
-export const updateUser: RequestHandler = async (req,res) =>{
-    const userUpdated = await User.findByIdAndUpdate(req.params.id, req.body,{new:true})
+export const updateUser: RequestHandler = async (req, res) => {
+    const userUpdated = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
     res.json(userUpdated);
 }
