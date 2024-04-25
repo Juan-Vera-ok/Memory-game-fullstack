7bf456951089e37a7f17d9d955e73338c90d792a
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-25 00:38:28 -0300

    Fixing config dabatase

--- Back/src/config.ts
+++ Back/src/config.ts
@@ -8,6 +8,7 @@ export default {
     MONGO_USER: process.env.USER ||"admin",
     MONGO_PASSWORD:process.env.MONGO_PASS|| "admin",
     MONGO_HOST:process.env.MONGO_HOST ||"localhost",
+    MONGO_PORT:process.env.MONGO_PORT,
     PORT: process.env.PORT || "3000",
     JWT_SECRET: getEnv('JWT_SECRET')
 }
--- Back/src/database.ts
+++ Back/src/database.ts
@@ -4,10 +4,10 @@ import config from "./config";
 (async ()=>{
     try {
         const mongooseOption:ConnectOptions = {
-            //user:config.MONGO_USER,
-            //pass:config.MONGO_PASSWORD
+            user:config.MONGO_USER,
+            pass:config.MONGO_PASSWORD
         }
-        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`)
+        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DATABASE}`,mongooseOption)
         console.log('Database is connected to',db.connection.name)
     } catch (error) {
         console.log(error)
