786fc494cdf78e12d90d8ad7357c7b3f5b68b505
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-26 00:37:48 -0300

    Fixing cors with * again

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -17,9 +17,6 @@ app.use(morgan('dev'));
 
 const corsOptions ={
     origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-    
 }
 app.use(cors(corsOptions));
 
