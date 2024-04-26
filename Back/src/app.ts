af6ecac88d90236b6105c4ce708e318fbda53daa
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-26 02:20:53 -0300

    Fix

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -22,15 +22,6 @@ const corsOptions ={
 }
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://memory-game-client.onrender.com');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
-
-
 app.use(userRoutes)
 
 export default app
