a1b5492bef7d031b1b9db1caffc1c0680ad72cea
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-26 02:15:44 -0300

    Fix

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -15,11 +15,11 @@ app.use(cookieParser())
 
 app.use(morgan('dev'));
 
-/*const corsOptions ={
+const corsOptions ={
     origin:'https://memory-game-client.onrender.com',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
-}*/
+}
 app.use(cors());
 
 app.use((req, res, next) => {
