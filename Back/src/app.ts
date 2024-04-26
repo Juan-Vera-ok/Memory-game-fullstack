ef55094374ab0a32d77d38f2e17ec45f27bdb0cc
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-26 02:21:38 -0300

    Fix

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -20,7 +20,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(userRoutes)
 
