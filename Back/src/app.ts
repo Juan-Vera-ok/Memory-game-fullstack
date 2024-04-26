2754354328a90bf20fa3d24a48fb5050b41233b9
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-26 00:02:00 -0300

    Fixing cors options

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -15,8 +15,13 @@ app.use(cookieParser())
 
 app.use(morgan('dev'));
 
-app.use(cors());
-
+const corsOptions ={
+    origin:'http://localhost:4000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+    
+}
+app.use(cors(corsOptions));
 
 
 app.use(userRoutes)
