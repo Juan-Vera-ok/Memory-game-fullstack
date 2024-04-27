a6444b5d1c69dd45dce7cee9e03884be7a5cec9b
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-27 19:48:44 -0300

    Fix

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -16,7 +16,7 @@ app.use(cookieParser())
 app.use(morgan('dev'));
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '/\memory-game-client\.onrender\.com$/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', "true");
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
