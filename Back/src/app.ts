2e2e89b0e7361744aeb93e37a2feb5aff5721a5c
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-04-28 01:29:14 -0300

    Fix

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    ["Content-Type", "Authorization", "X-Content-Type-Options", "Accept", "X-Requested-With", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"]
+    ["Access-Control-Allow-Origin","Content-Type", "Authorization", "X-Content-Type-Options", "Accept", "X-Requested-With", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"]
   );
   res.setHeader("Access-Control-Allow-Credentials", 'true');
   res.setHeader("Access-Control-Allow-Private-Network", 'true');
