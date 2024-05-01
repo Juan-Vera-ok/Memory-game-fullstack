674fa0da968494edf64e644c1b385b00d21e1ee0
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-05-01 19:15:38 -0300

    Fix

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
 */
 
 app.use(cors({
-  origin: "https://memory-game-client.onrender.com",
+  origin: "memory-game-client.onrender.com",
   credentials: true,
   allowedHeaders: ["Content-Type",
   "Authorization",
