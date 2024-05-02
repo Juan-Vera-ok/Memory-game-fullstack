c4717d430dad231a635af1897ca642298768c66c
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-05-02 00:53:38 -0300

    Fix

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', ['https://memory-game-client.onrender.com']);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,application/x-www-form-urlencoded,multipart/form-data,text/plain");
   res.header('Access-Control-Allow-Credentials','true')
   next();
 });
