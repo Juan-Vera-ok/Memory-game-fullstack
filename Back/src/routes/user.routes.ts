85260c0d22286e3d8a4bdee099318e575dd4432c
Juan-Vera-ok <juanvera2001@hotmail.com>
2024-05-02 22:28:59 -0300

    Fix

--- Back/src/routes/user.routes.ts
+++ Back/src/routes/user.routes.ts
@@ -8,12 +8,7 @@ router.get('/users', userController.getUsers);
 router.post('/userAuth',userController.auth);
 router.delete('/user/:id', userController.deleteUser);
 router.put('/user/:id', userController.updateUser);
-router.post('/update-highscore',cors({origin:"https://memory-game-client.onrender.com",
-  credentials:true
-}),userController.updateHighScore
- );
-router.post('/highScore-current-user',cors({origin:"https://memory-game-client.onrender.com",
-credentials:true
-}),userController.highScoreOfCurrentUser)
+router.post('/update-highscore',userController.updateHighScore);
+router.post('/highScore-current-user',userController.highScoreOfCurrentUser)
 
 export default router
